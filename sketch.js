98805f4fb5b9b73f4d26077e2cdb6cbe1d7f46c0
NicolaGuerrera <nicolaguerrera23@libero.it>
2025-03-25 12:49:07 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,10 +11,25 @@ function draw() {
   rect(width - s, 0, s, s);
   rect(width - s, height - s, s, s);
 
+  push();
   textSize(80);
   textAlign("center");
-  text("Nicola", width / 2, height / 2);
   fill("red");
+  text("Nicola", width / 2, height / 2);
+  pop();
+
+  let facepositionvertical = height / 2 - 20;
+  let faceSize = 80;
+  let faceX = width / 2 + 170;
+  let faceY = facepositionvertical;
+
+  fill(0);
+  ellipse(faceX - 20, faceY - 10, 10, 10);
+  ellipse(faceX + 20, faceY - 10, 10, 10);
+
+  noFill();
+  strokeWeight(3);
+  arc(faceX, faceY + 10, 40, 20, 0, PI);
 }
 
 function windowResized() {
